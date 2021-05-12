363fb4cbd4839ad3c35d24305b4a66052c01781a
hexadeci-male <joeisnomoe@mail.com>
2021-05-11 23:32:31 -0600

    Cleanup and added fields attribute

--- cnw_scraper/profile.py
+++ cnw_scraper/profile.py
@@ -1,13 +1,32 @@
 class Profile:
     """
-    Contains information from a subject's net worth page (print me for a pretty display of my contents). This object isn't created directly, but rather is made through the api functions.\n
-    :stats: Dictionary containing name, net worth, salary, gender, nationality, etc. - Net Worth gets printed out as a dollar amount, but is actually stored as a real number string.\n
-    :description: String containing the bio of the subject - optional.\n
+    An object that ontains information from a subject's net worth page (print me for a pretty display of my contents). This object isn't created directly, but rather is made through the scrape functions.
+
     Note: Certain stats for certain profiles might not exist, as the profile simply doesn't have them on the site. A profile will always have at least a name and net worth.
+
+    :stats: Dictionary containing name, net worth, salary, gender, nationality, etc. - Net Worth gets printed out as a dollar amount, but is actually stored as a real number string.
+
+    :description: String containing the bio of the subject - optional.
+
+    :fields: Static class attribute. Helper list showing all known stats that make up any profile on the site. Useful for writing these as columns to CSV so you don't have to add them yourself through trial and error. These might change in the future as the site may or may not update the profiles with more stats.
     """
+
+    fields = [
+        "Name",
+        "Net Worth",
+        "Salary",
+        "Date of Birth",
+        "Gender",
+        "Height",
+        "Profession",
+        "Nationality",
+        "Last Updated",
+    ]
+
     def __init__(self, stats:dict, description:str):
         self.stats = stats
         self.description = description
+
     def __str__(self):
         stats = dict(self.stats)
         stats["Net Worth"] = f"${int(stats['Net Worth']):,}"
